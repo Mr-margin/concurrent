2a555175b2e86d44fb5d465711775c1cabbb67cb
Mr丶空白 <cd230515@gmail.com>
2019-02-25 17:32:41 +0800

    Update Test_01_Myexcutor.java

--- src/com/cd/concurrent/t08/Test_01_Myexcutor.java
+++ src/com/cd/concurrent/t08/Test_01_Myexcutor.java
@@ -8,7 +8,6 @@ import java.util.concurrent.Executor;
  * 在使用线程池的时候，底层如何调用线程汇总的逻辑
  */
 public class Test_01_Myexcutor implements Executor {
-
     public static void main(String[] args){
         new Test_01_Myexcutor().execute(new Runnable() {
             @Override
